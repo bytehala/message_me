6d67b6a0e3044f438d21fdfb85c34bb4673ba812
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-03-03 01:19:22 -0700

    Fix bug where has_many was not correct object

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maxuimum: 15 }
-    has_many :Mm
+    has_many :messages
     has_secure_password
 end
